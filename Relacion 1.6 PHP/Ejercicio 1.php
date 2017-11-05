8f2c2472827d35aca24d22abaa83c761d28a94ea
Jesus Estevez <32360649+jEstevezRod@users.noreply.github.com>
2017-11-05 21:28:27 +0100

    update exe 1 php

--- Relacion 1.6 PHP/Ejercicio 1.php
+++ Relacion 1.6 PHP/Ejercicio 1.php
@@ -5,8 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Formulario</title>
+    <style>
+        form {
+
+            background-color: lightblue;
+            margin: 400px;
+            margin-top: 50px !important;
+            
+        }
+    
+    </style>
 </head>
 <body>
+
+<form method="post">
+
+    <fieldset>
+        <label>
+            <b>Nombre : </b>
+            <input type="text" name="nombre">
+        </label>
+    </fieldset>
+
+
+    <fieldset>
+        <label>
+            <b>Apellido : </b>
+            <input type="text" name="apellido">
+        </label>
+    </fieldset>
+
+
+    <fieldset>
+        <label>
+            <b>Teléfono : </b>
+            <input type="text" name="telefono">
+        </label>
+    </fieldset>
+
+
+    <fieldset>
+        <button type="submit" name="enviar">Enviar</button>
+    </fieldset>
+
+</form>
+
 <?php
 $nombre = "";
 if (!empty($_POST['nombre'])) {
@@ -16,7 +59,7 @@ $apellidos = "";
 if (!empty($_POST['apellido'])) {
     $apellidos = $_POST['apellido'];
 }
-$movil = "";
+$telefono = "";
 if (!empty($_POST['telefono'])) {
     $telefono = $_POST['telefono'];
 }
@@ -25,37 +68,6 @@ $file = @fopen($archivo, "a");
 fwrite($file, 'Nombre: ' . $nombre . ' Apellido: ' . $apellidos . ' Telefono: ' . $telefono);
 fclose($file);
 ?>
-<form method="post">
-
-    <fieldset>
-        <label>
-            <b>Nombre - </b>
-            <input type="text" name="nombre">
-        </label>
-    </fieldset>
-
-
-    <fieldset>
-        <label>
-            <b>Apellido - </b>
-            <input type="text" name="apellido">
-        </label>
-    </fieldset>
-
-
-    <fieldset>
-        <label>
-            <b>Teléfono - </b>
-            <input type="text" name="telefono">
-        </label>
-    </fieldset>
-
-
-    <fieldset>
-        <button type="submit" name="enviar">Insertar</button>
-    </fieldset>
-
-</form>
 
 </body>
 </html>
