aed523fa33dc5268c7a4e2be0b25fa88515abd22
Jesus Estevez <32360649+jEstevezRod@users.noreply.github.com>
2017-11-06 10:13:27 +0100

    exer adiv num

--- Relacion 1.6 PHP/Ejercicio 9.php
+++ Relacion 1.6 PHP/Ejercicio 9.php
@@ -1,23 +1,50 @@
-<html>
-<body>
-<form action="" method="post">
-    ¿Adivinas el número? (Entre 0 y 100):<br>
-    <input type="number" name="comprobar"><br>
-    <input type="hidden" name="random" value="<?php echo $random; ?>">
-    <input type="submit" name="action" value="Comprobar">
-</form>
-<?php
+<!doctype html>
+<html lang="en">
 
-$random = isset($_POST['random']) ? $_POST['random'] : rand(0, 100);
-if (isset($_POST['comprobar'])) {
-    if ($_POST['comprobar'] == $random) {
-        echo "Acertaste<br/>Hemos generado un nuevo número aleatorio.<br/>";
-        $random = rand(0, 100);
-    } else {
-        echo "Pruebe suerte otra vez";
+<head>
+	<meta charset="UTF-8">
+	<meta name="Generator" content="EditPlus®">
+	<meta name="Author" content="">
+	<meta name="Keywords" content="">
+	<meta name="Description" content="">
+	<title>Ejercicio 9</title>
+</head>
+
+	<body>
+
+		<form method="post" />
+
+	    <p>Numero: <input type="text" name="adivina"/>
+			<p><input type="hidden" name="num"/ value=$num>
+			<p><input type="hidden" name="intentos"/>
+
+	    <p><input type="submit" value="Comprobar el formulario">
+	       <input type="reset" value="borrar todo">
+	    </p>
+
+	  </form>
+  </br>
+
+		<?php
+
+		function setRandom(){
+		   $res = rand(0, 100);
+		   setcookie("random", $res, time() + 60 * 60 * 24 * 100, "/");
+		   return $res;
+		}
+
+		$random = isset($_COOKIE['random']) ? $_COOKIE['random'] : setRandom();
+		echo $random.'</br>';
+
+	  if (!empty($_POST['adivina'])){
+	  $num = $_POST['adivina'];
+			if ($random > $num) print('El numero es mayor.' . $num);
+			elseif ($random < $num) print('El numero es menor.' . $num);
+			elseif ($random == $num) print('Enhorabuena, ese es el numero correcto.');
     }
-}
-?>
 
-</body>
+		?>
+
+	</body>
+
 </html>
